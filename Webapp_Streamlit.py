570b65989ecee5a996baed3ba3afd51b0455d0dd
Gabriel Guedes <gabriellguedes16@gmail.com>
2022-01-17 18:07:12 -0300

    Atualização da aplicação

--- Webapp_Streamlit.py
+++ Webapp_Streamlit.py
@@ -3,9 +3,10 @@ import pandas as pd
 import streamlit as st
 import surprise
 import os
+from sk import corte_pivo_log
 from collections import defaultdict
 
-#Loading model
+#Loading model and dataframes
 model = pickle.load(open(os.getcwd()+"/.pkl/svdpp.pkl", 'rb'))
 offer_title = pickle.load(open(os.getcwd()+'/.pkl/offers.pkl', 'rb'))
 df = pd.read_csv(os.getcwd()+'/sparse_melt.csv')
@@ -48,23 +49,23 @@ def recommendations_from_SVDpp(user_id, pivo_log, algo, n_recommendations):
 
 		top_n_recommendations[user_id] = ratings[:n_recommendations]
 
+
+	#print("Recommendations for user with id {}: ".format(user_id))
+
+	msg = "Because you clicked items like:\n\n"
+	for item in list_of_clicked_items.ra[:5]:
+		msg = msg+offer_title[item]+'\n\n'
+
+	msg+=' We recommend:\n\n'
+
 	count = 0
-
-	print("Recommendations for user with id {}: ".format(user_id))
-
-	#msg1 = "Because you clicked:\n"
-	#for i in list_of_clicked_items:
-	#	msg1+=i
-
-	message = ''
 
 	for item_index, score in top_n_recommendations[user_id]:
 		count += 1
 
-		message += (str(count) + '. ' + str(offer_title[item_index]) + ' predicted rating = ' + str(round(score, 3)))
-		message += '\n'
-	#message = msg1+message
-	return categorias_usuario
+		msg += (str(count) + '. ' + str(offer_title[item_index]) + ' predicted rating = ' + str(round(score, 3)))
+		msg += '\n'
+	return msg
 
 
 def main():
